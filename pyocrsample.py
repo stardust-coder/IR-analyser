2b789662aec8f477a8b5373f30b9a3a1f53b8091
stardust-coder <stardust13551@gmail.com>
2020-08-08 22:43:50 +0900

    graph visualization added

--- pyocrsample.py
+++ pyocrsample.py
@@ -38,7 +38,7 @@ def totext(imagefilename):
         print("OCR has failed!")
         return []
 
-    finaldata = []
+    finaldata = {}
     for row in results:
         item = re.sub(" ","",re.match("[^0-9０-９]+", row).group())
         item = re.sub("央定","固定",item)
@@ -47,12 +47,69 @@ def totext(imagefilename):
             str1 = "".join(cost[:int(len(cost)/2)])
             str2 = "".join(cost[int(len(cost)/2):])
             cost = [str1,str2]
-        finaldata.append([item,cost[0],cost[1]])
+        finaldata[item] = [cost[0],cost[1]]
     return finaldata
 
-assets_extracted = totext(assets)
-liabilities_extracted = totext(liabilities)#トヨタでバグるなぁ
-
+assets_extracted = totext(assets)#辞書型
+liabilities_extracted = totext(liabilities)
+total = int(assets_extracted["資産合計"][0])#liabilities[-1][1]と同じ値のはず
 
 print(assets_extracted)
 print(liabilities_extracted)
+print(total)
+
+year = 1 #前事業年度なら0, 今事業年度なら1
+
+####グラフ描画
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(font='IPAexGothic')
+assets_index = []
+assets_rawdata = []
+liabilities_index = []
+liabilities_rawdata = []
+
+for element in ["固定資産合計","流動資産合計"]:
+    #name = assets_extracted[element][0]
+    amount = assets_extracted[element][year]
+    assets_index.append(element)
+    assets_rawdata.append([int(amount),0])
+
+for element in ["純資産合計","固定負債合計", "流動負債合計"]:
+    #name = element[0]
+    amount = liabilities_extracted[element][year]
+    liabilities_index.append(element)
+    liabilities_rawdata.append([0,int(amount)])
+
+rawindex = assets_index + liabilities_index
+rawdata = assets_rawdata + liabilities_rawdata
+
+#プロット用データに加工
+dataset = pd.DataFrame(rawdata,
+                       columns=['借方', '貸方'],
+                       index=rawindex)
+
+print(dataset)#データセット確認
+
+
+#プロット！
+fig, ax = plt.subplots(figsize=(10, 8))
+for i in range(len(dataset)):
+    ax.bar(dataset.columns, dataset.iloc[i], bottom=dataset.iloc[:i].sum())
+    for j in range(len(dataset.columns)):
+        itemlabelnum = dataset.iloc[i, j]
+        itemlabeltext = dataset.index[i]
+        if itemlabelnum/total >= 0.05:#小さすぎる文字を表示しないように
+            #ラベル文字のオプション設定
+            plt.text(x=j,
+                     y=dataset.iloc[:i, j].sum() + (dataset.iloc[i, j] / 2),
+                     s= itemlabeltext + " " + str(itemlabelnum),
+                     ha='center',
+                     va='bottom',
+                     color = "white"
+                    )
+ax.set(xlabel='バランスシート', ylabel='額（円）')
+#ax.legend(dataset.index)#凡例は不要
+plt.show()
